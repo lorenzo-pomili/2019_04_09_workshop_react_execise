b3d056df6b020183b70b34e4bcac9fa9ebcf3865
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2019-04-08 21:55:33 +0200

    add static list

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const Header = () => <h1>Todo List</h1>;
 ReactDOM.render(
     <div>
       <Header />
+      <div>
+        <div><span>Foo</span><span style={{marginLeft: "50px", cursor: "pointer"}}>x</span></div>
+        <div><span>Bar</span><span style={{marginLeft: "50px", cursor: "pointer"}}>x</span></div>
+        <div><span>Baz</span><span style={{marginLeft: "50px", cursor: "pointer"}}>x</span></div>
+      </div>
     </div>,
     document.getElementById('root')
 );
