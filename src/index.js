cc2025d1eba0a86cfae3d15d8d32e95dac316623
lorenzo.pomili <pomili.lorenzo85@gmail.com>
2019-04-08 22:03:08 +0200

    add dynamic list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const listElements = [
+  {id: 1, label: "Foo", isDone: false},
+  {id: 2, label: "Bar", isDone: false},
+  {id: 3, label: "Baz", isDone: false}
+];
+
 const Header = () => <h1>Todo List</h1>;
+
+const ListElement = ({label, isDone}) => <div>
+  <span>{label}</span>
+  <span style={{marginLeft: "50px", cursor: "pointer"}}>{isDone ? 'v' : 'x'}</span>
+</div>;
 
 ReactDOM.render(
     <div>
       <Header />
       <div>
-        <div><span>Foo</span><span style={{marginLeft: "50px", cursor: "pointer"}}>x</span></div>
-        <div><span>Bar</span><span style={{marginLeft: "50px", cursor: "pointer"}}>x</span></div>
-        <div><span>Baz</span><span style={{marginLeft: "50px", cursor: "pointer"}}>x</span></div>
+        {listElements.map((e, i) =>
+          <ListElement key={i} {...e} />
+        )}
       </div>
     </div>,
     document.getElementById('root')
